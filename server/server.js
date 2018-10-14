84067216dc17f89e9dee5b6da06080d9bffee218
Daniel Cunha <danielcunha54@gmail.com>
2018-10-14 12:22:21 -0300

    Add POST /users route

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 require('./config/config');
 
+const _ = require('lodash');
 const express = require('express');
 const bodyParser = require('body-parser');
 
@@ -11,7 +12,14 @@ const port = process.env.PORT;
 
 app.use(bodyParser.json())
 
+app.post('/users', async (req, res) => {
+    let user = new User(_.pick(req.body, ['email', 'password']));
 
+    user.save()
+        .then(() => user.generateAuthToken())
+        .then(token => res.header('x-auth', token).send(user))
+        .catch(e => res.status(400).send(e));
+});
 
 app.listen(port, () => console.log(`Server is up on port ${port}.`));
 
