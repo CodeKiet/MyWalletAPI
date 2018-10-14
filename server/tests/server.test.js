ad566a4929074f846da4eb687ecec48138cf4bca
Daniel Cunha <danielcunha54@gmail.com>
2018-10-14 13:33:38 -0300

    Add test cases for DELETE /users/me/token

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -142,3 +142,21 @@ describe('GET /users/me', () => {
             .end(done);
     });
 });
+
+describe('DELETE /users/me/token', () => {
+    it('should remove auth token on logout', done => {
+        request(app)
+            .delete('/users/me/token')
+            .set('x-auth', _baseUsers[0].tokens[0].token)
+            .expect(200)
+            .end((err, res) => {
+                if (err)
+                    return done(err);
+
+                User.findById(_baseUsers[0]._id).then(user => {
+                    expect(user.tokens.length).toBe(0);
+                    done();
+                }).catch(e => done(e));
+            });
+    });
+});
