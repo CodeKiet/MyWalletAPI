2bbee1e8bf7e1ab86e3d6dc7305773edc72088a6
Daniel Cunha <danielcunha54@gmail.com>
2018-10-14 13:29:35 -0300

    Add test cases for GET /users/me route

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -120,3 +120,25 @@ describe('POST /users/login', () => {
             });
     });
 });
+
+describe('GET /users/me', () => {
+    it('should return user if authenticated', done => {
+        request(app)
+            .get('/users/me')
+            .set('x-auth', _baseUsers[0].tokens[0].token)
+            .expect(200)
+            .expect(res => {
+                expect(res.body._id).toBe(_baseUsers[0]._id.toHexString());
+                expect(res.body.email).toBe(_baseUsers[0].email);
+            })
+            .end(done);
+    });
+
+    it('should return 401 if not authenticated', done => {
+        request(app)
+            .get('/users/me')
+            .expect(401)
+            .expect(res => expect(res.body).toEqual({}))
+            .end(done);
+    });
+});
